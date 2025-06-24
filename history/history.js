c2c8b883c9ff65e13247b82c969368154ccb2f89
Maxime Guerin <maxiguerin348@gmail.com>
2025-06-24 20:06:21 +0200

    Good Historique

--- history/history.js
+++ history/history.js
@@ -145,12 +145,18 @@ function createHistoryRow(day, index) {
     if (dailyTarget > 0) {
         if (day.count >= dailyTarget) {
             statusText = `<span class="status-badge status-success">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"><path fill-rule="evenodd" d="M8 15a7 7 0 1 0 0-14 7 7 0 0 0 0 14m3.04-9.96a.75.75 0 0 0-1.08-1.04l-3.25 3.5-1.5-1.5a.75.75 0 0 0-1.06 1.06l2 2a.75.75 0 0 0 1.06 0z" clip-rule="evenodd" /></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+                              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                              <path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05"/>
+                            </svg>
                             Atteint
                           </span>`;
         } else {
             statusText = `<span class="status-badge status-failure">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"><path fill-rule="evenodd" d="M8 15a7 7 0 1 0 0-14 7 7 0 0 0 0 14m-1.03-9.97a.75.75 0 0 1 1.06-1.06L8 5.94l1.97-1.97a.75.75 0 1 1 1.06 1.06L9.06 7l1.97 1.97a.75.75 0 1 1-1.06 1.06L8 8.06l-1.97 1.97a.75.75 0 1 1-1.06-1.06L6.94 7l-1.97-1.97z" clip-rule="evenodd" /></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+                              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+                            </svg>
                             Non atteint
                           </span>`;
         }
